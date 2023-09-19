d3b5d2635deea1543591e914af8edadb8a63ede9
Konosuke Sakai <konosuke@konosuke.dev>
2023-09-19 15:18:49 +0900

    :sparkles: implement the core

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import subprocess
 
 class SpeedtestClient(discord.Client):
     async def on_ready(self):
@@ -7,6 +8,10 @@ class SpeedtestClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+
+        if message.content == '/speedtest':
+            speedtest = subprocess.run(['speedtest'], capture_output=True, text=True)
+            await message.channel.send(f'```\n{speedtest.stdout}\n```')
 
 intents = discord.Intents.default()
 intents.message_content = True
